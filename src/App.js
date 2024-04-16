3579ccf6c1cea2273e4f577fa294b2367c4be039
Devansh <devanshaaggarwal@gmail.com>
2024-04-16 15:43:39 +0530

    "Initial Commit"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [note, setNote] = useState('');
+  const [currentNotes, setCurrentNotes] = useState([]);
+
+  const handleClick = () => {
+    setCurrentNotes([...currentNotes, note])
+    console.log(currentNotes)
+  }
+
+  const handleChange = (event) => {
+    setNote(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quill</h1>
+      <p>Capture your thoughts with elegance</p>
+      <div className='form'>
+        <div className='mb-3'>
+          <label htmlFor='note' className='form-label'>Note :</label>
+          <input
+            type='test'
+            className='form-control'
+            id='note'
+            name='note'
+            placeholder='Enter your note here...'
+            onChange={handleChange}
+            value = {note}
+          />
+          <br></br>
+          <button type="button" class="btn btn-outline-primary" onClick={handleClick} >Submit</button>
+        </div>
+      </div>
+
+      <div className='mb-3'>
+        {
+          currentNotes.length === 0
+          ?
+          <p>No notes added yet.</p>
+          :
+          <table>
+            <thead>
+              <th>Note</th>
+            </thead>
+            <tbody>
+              {
+                currentNotes.map((n) => (
+                  <tr>
+                    <td>{n}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        }
+      </div>
+
     </div>
   );
 }
